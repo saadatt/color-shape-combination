21ecc49cc5a2bd6fbfe35c0b77cb116314e643e8
saadatt <saadatt@gmail.com>
2022-03-21 16:37:49 -0500

    updated ShapeTaskScreen.tsx and css files

--- src/screens/ShapeTaskScreen.tsx
+++ src/screens/ShapeTaskScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, Fragment} from "react";
 import {Circle, Layer, Rect, Stage, Star, RegularPolygon} from "react-konva";
 import './ShapeTaskScreen.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -8,34 +8,34 @@ import moment from "moment";
 
 type SelectableShape = 'rectangle' | 'circle' | 'triangle' | 'star'
 type Shape = SelectableShape | 'square'
-type ShapeColor = 'blue' | 'yellow' | 'red' | 'green'
+type ShapeColor = 'red' | 'yellow' | 'blue' | 'green'
 
-export const POSSIBLE_SHAPE_OUTCOMES: { [key in ShapeColor]: { [key in SelectableShape]: [ShapeColor, Shape] } } = {
-    blue: {
-        'rectangle': ['blue', 'rectangle'],
-        'circle': ['blue', 'circle'],
-        'triangle': ['blue', 'triangle'],
-        'star': ['blue', 'star'],
-    },
-    yellow: {
-        'rectangle': ['yellow', 'rectangle'],
-        'circle': ['yellow', 'circle'],
-        'triangle': ['yellow', 'triangle'],
-        'star': ['yellow', 'star'],
-    },
-    red: {
-        'rectangle': ['green', 'rectangle'],
-        'circle': ['yellow', 'circle'],
-        'triangle': ['yellow', 'triangle'],
-        'star': ['red', 'star'],
-    },
-    green: {
-        'rectangle': ['green', 'rectangle'],
-        'circle': ['green', 'circle'],
-        'triangle': ['green', 'triangle'],
-        'star': ['green', 'star'],
-    },
-}
+const RESULT_CASES: [ShapeColor, Shape][] = [
+    //00 red
+    ['green', 'rectangle'],   //00 rectangle => 0 << 2 | 0 ==> index = 0  0000
+    ['yellow', 'circle'],   //10 circle =>      0 << 2 | 1 ==> index = 1  0010
+    ['yellow', 'triangle'],   //01 triangle =>  0 << 2 | 2 ==> index = 2  0001
+    ['red', 'star'],   //11 star =>             0 << 2 | 3 ==> index = 3  0011
+    //10 yellow
+    ['yellow', 'rectangle'],   //00 rectangle => 1 << 2 | 0 ==> index = 4
+    ['yellow', 'circle'],   //10 circle =>       1 << 2 | 1 ==> index = 5
+    ['yellow', 'triangle'],   //01 triangle =>   1 << 2 | 2 ==> index = 6
+    ['yellow', 'star'],   //11 star =>           1 << 2 | 3 ==> index = 7
+    //01 blue
+    ['blue', 'rectangle'],   //00 rectangle => 2 << 2 | 0 ==> index = 8
+    ['blue', 'circle'],   //10 circle =>      2 << 2 | 1 ==> index = 9
+    ['blue', 'triangle'],   //01 triangle =>  2 << 2 | 2 ==> index = 10
+    ['blue', 'star'],   //11 star =>             2 << 2 | 3 ==> index = 11
+    //11 green
+    ['green', 'rectangle'],   //00 rectangle => 3 << 2 | 0 ==> index = 12
+    ['green', 'circle'],   //10 circle =>      3 << 2 | 1 ==> index = 13
+    ['green', 'triangle'],   //01 triangle =>  3 << 2 | 2 ==> index = 14
+    ['green', 'star'],   //11 star =>             3 << 2 | 3 ==> index = 15
+
+];
+
+const ALL_COLORS = ['red', 'yellow', 'blue', 'green'] as const;
+const ALL_SHAPES = ['rectangle', 'circle', 'triangle', 'star'] as const;
 
 interface EvaluationRecord {
     input: [ShapeColor, SelectableShape]
@@ -44,30 +44,70 @@ interface EvaluationRecord {
 }
 
 export function getShapeAndColorForInput(color: ShapeColor, shape: SelectableShape): [ShapeColor, Shape] {
-    return POSSIBLE_SHAPE_OUTCOMES[color][shape]
+    const colorIdx = ALL_COLORS.indexOf(color);
+    const shapeIdx = ALL_SHAPES.indexOf(shape);
+    const resultIdx = colorIdx << 2 | shapeIdx;
+    return RESULT_CASES[resultIdx];
 }
 
-const getColorHex = (color: ShapeColor) => Colors[color]
-const renderShape = (shape: Shape, x: number, y: number, onClick?: () => any, color?: ShapeColor) => {
+const getColorHex = (color: ShapeColor) => Colors[color];
 
+interface ShapeProps {
+    shape: Shape;
+    isSelected?: boolean;
+    onClick?: () => void;
+    color?: ShapeColor;
+    className?: string;
+}
+
+const Shape = (props: ShapeProps) => {
+    const {onClick, shape, color, isSelected, className} = props;
     const fillColor = color ? getColorHex(color) : undefined;
-    const result = shape === 'rectangle'
-        ?  <Rect fill={fillColor} onClick={onClick} y={y} x={x} width={100} height={50} stroke="black"/>
-        : shape === 'square'
-            ? <Rect fill={fillColor} onClick={onClick} y={y} x={x} width={50} height={50} stroke="black"/>
-            : shape === 'circle'
-                ? <Circle fill={fillColor} onClick={onClick} x={x} y={y} stroke="black" radius={25}/>
-                : shape ==='star'
-                    ? <Star fill={fillColor} onClick={onClick} x={x} y={y} stroke="black" radius={25} numPoints={5}
-                            innerRadius={15}
-                            outerRadius={30}/>
-                    : shape ==='triangle'
-                        ? <RegularPolygon fill={fillColor} onClick={onClick} x={x} y={y} stroke="black" radius={35} sides={3} />
-                        : null
-    return result;
-}
+    const resultTable = {
+        rectangle: {
+            width: 100,
+            height: 50,
+            markup: <Rect fill={fillColor} y={0} x={0} width={100} height={50} stroke="black"/>
+        },
+        square: {
+            width: 50,
+            height: 50,
+            markup: <Rect fill={fillColor} y={0} x={0} width={50} height={50} stroke="black"/>
+        },
+        circle: {
+            width: 50,
+            height: 50,
+            markup: <Circle fill={fillColor} x={25} y={25} stroke="black" radius={24}/>
+        },
+        star: {
+            width: 50,
+            height: 50,
+            markup: <Star fill={fillColor} x={25} y={25} stroke="black" radius={25} numPoints={5} innerRadius={10}
+                          outerRadius={25}/>
+        },
+        triangle: {
+            width: 50,
+            height: 50,
+            markup: <RegularPolygon fill={fillColor} x={25} y={30} stroke="black" radius={25} sides={3}/>
+        }
+    } as const;
 
-const ShapeTaskScreen=() => {
+    const {width, markup, height} = resultTable[shape];
+
+    return (
+        <div className={className || 'shape-wrapper'}>
+            <Stage width={width} height={height} onClick={onClick} className={'shape-container'}>
+                <Layer>{markup}</Layer>
+            </Stage>
+            {isSelected ? <FontAwesomeIcon className="color-picker__check-mark"
+                                           icon={faCheck} color={'green'}
+                                           size={'2x'}/> : null}
+        </div>
+    );
+};
+
+
+const ShapeTaskScreen = () => {
     const [selectedColor, setSelectedColor] = useState<ShapeColor | undefined>('red');
     const [records, setRecords] = useState<EvaluationRecord[]>([]);
     const [latestMomentChecked, setLatestMomentChecked] = useState(moment())
@@ -85,17 +125,25 @@ const ShapeTaskScreen=() => {
     const renderColorPicker = (color: ShapeColor) => {
         const isSelected = selectedColor === color
         const colorPickerClassnames = 'color-picker mx-3' + (isSelected ? 'color_picker_checked' : '')
-        return <div onClick={() => setSelectedColor(color)}
-                    className={colorPickerClassnames}
-                    style={{backgroundColor: getColorHex(color)}}>
-            {selectedColor === color ?
-                <FontAwesomeIcon className='color-picker__check-mark'
-                                 icon={faCheck} color={'green'}
-                                 size={'2x'}/> : null}
-        </div>
-    }
+        return (
+            <div
+                key={color}
+                onClick={() => setSelectedColor(color)}
+                className={colorPickerClassnames}
+                style={{backgroundColor: getColorHex(color)}}>
+                {selectedColor === color ? (
+                    <FontAwesomeIcon
+                        className='color-picker__check-mark'
+                        icon={faCheck}
+                        color={'green'}
+                        size={'2x'}/>
+                ) : null}
+            </div>
+        );
+    };
+
     const onShapeClicked = (shape: SelectableShape) => {
-         if (selectedColor){
+        if (selectedColor) {
             const newRecord: EvaluationRecord = {
                 input: [selectedColor, shape],
                 output: getShapeAndColorForInput(selectedColor, shape),
@@ -103,58 +151,38 @@ const ShapeTaskScreen=() => {
             }
             setRecords([newRecord].concat(records))
         }
-    }
+    };
+
+    const renderShape = (shape: SelectableShape) => {
+        const isSelected = records.length > 0 && records[0].input[1] === shape;
+        return <Shape key={shape} shape={shape} onClick={() => onShapeClicked(shape)} isSelected={isSelected}/>
+    };
 
     return (
-        <div className='shape-screen-container px-5 py-5'>
+        <div className='shape-screen-container'>
             <div className='pt-5'/>
-            <h3 style={{textAlign: 'center'}}>Please click on shape and color</h3>
-            <div className='d-flex flex-row justify-content-center mt-4'>
-                {renderColorPicker('red')}
-                {renderColorPicker('yellow')}
-                {renderColorPicker('blue')}
-                {renderColorPicker('green')}
+            <h3 style={{textAlign: 'center'}}>Please pick your shape and color</h3>
+            <div className='selection-table'>
+                {ALL_COLORS.map(renderColorPicker)}
+                {ALL_SHAPES.map(renderShape)}
             </div>
 
-            <div className='row'>
-                <div className='col-2 col-sm-1'/>
-                <Stage className='col-12 col-lg-8 d-flex justify-content-center' width={window.innerWidth}
-                       height={300}>
-                    <Layer>
-                        {renderShape('rectangle', 100, 100, () => onShapeClicked('rectangle'))}
-                        {renderShape('circle', 330, 125, () => onShapeClicked('circle'))}
-                        {renderShape('triangle', 500, 125, () => onShapeClicked('triangle'))}
-                        {renderShape('star', 700, 125, () => onShapeClicked('star'))}
-                    </Layer>
-                </Stage>
-                <div className='col-2 col-sm-1'/>
-            </div>
-
-            {
-                records.length === 0
-                    ? null
-                    : <div className='evaluation-records-table col-8 mt-3'>
-                        {
-                            records.map((record, idx) => {
-                                    const [color, shape] = record.output
-                                    return <div key={idx}
-                                                className='py-2 px-2 d-flex flex-row justify-content-center align-items-center'>
-                                        <div className='col-auto'>
-                                            {`Color ${record.input[1]} and Shape ${record.input[0]} selected. Result is: `}
-                                        </div>
-                                        <Stage className='col-auto d-flex justify-content-center' width={200} height={100}>
-                                            <Layer>
-                                                {renderShape(shape, 50, 50, () => {
-                                                }, color)}
-                                            </Layer>
-                                        </Stage>
-                                    </div>
-                                }
-                            )
-                        }
-                    </div>
-            }
+            {records.length === 0 ? null : (
+                <div className='evaluation-records-table'>
+                    {records.map((record, idx) => {
+                        const [color, shape] = record.output;
+                        return (
+                            <Fragment key={idx}>
+                                <div className='col-auto' style={{fontFamily:'sans-serif'}}>
+                                    {`Shape ${record.input[1]} and Color ${record.input[0]} selected. Result is: `}
+                                </div>
+                                <Shape shape={shape} color={color}/>
+                            </Fragment>
+                        );
+                    })}
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 export default ShapeTaskScreen;
